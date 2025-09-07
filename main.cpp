4d8bd2b8915485d5b08a55968c353d98e1c9a6d5
Kwon <ggiicchh48@gmail.com>
2025-09-08 00:47:25 +0900

    protobuf adding

--- main.cpp
+++ main.cpp
@@ -5,26 +5,25 @@
 #include <random>
 #include "./game/game.h"
 #include "gameControl.h"
+#include "./proto/Match.pb.h"
 
-struct NewMatchRequest // 랜덤 입장
-{
-    
-};
 
-struct NewMatchInfo // 코드 입장 : 서버 -> 클라이언트
-{
-    int gameCode;
-};
-
-struct NewMatchResponse // 코드 입장 : 클라이언트 -> 서버
-{
-    int gameCode;
-
-};
 
 int main() {
     GameBoard gb;
     // 소켓 열기
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+
+int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in address{};
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(9090);
+
+    bind(server_fd, (struct sockaddr*)&address, sizeof(address));
+    listen(server_fd, 3);
+
 
     return 0;
 }
